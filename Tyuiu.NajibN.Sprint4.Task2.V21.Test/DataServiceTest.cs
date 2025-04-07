2dc8ab8e74fb23488e476e0f31aa06fb2fde5974
нассер <@NASSER>
2025-04-07 11:52:19 +0500

    Task2

---
+++ Tyuiu.NajibN.Sprint4.Task2.V21.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.NajibN.Sprint4.Task2.V21.Lib;
+namespace Tyuiu.NajibN.Sprint4.Task2.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void Valid()
+        {
+            DataService ds = new DataService();
+
+            int[] numsArray = { 1, 5, 4, 3, 5, 5, 2, 6, 3, 4, 5, 6, 2, 4, 5 };
+
+            int res = ds.Calculate(numsArray);
+            int wait = 9216;
+
+            Assert.AreEqual(wait, res);
+            {
+            }
+        }
+    }
+}
---
+++ Tyuiu.NajibN.Sprint4.Task2.V21.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.NajibN.Sprint4.Task2.V21.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.NajibN.Sprint4.Task2.V21.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("89733cfb-e85f-4bc7-82ae-51033567bb8b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
